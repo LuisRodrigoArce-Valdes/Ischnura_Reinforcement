49e903aedea378e87e5da362ba14662658a670bc
Luis Rodrigo Arce-Valdés <bio.l.rodrigo.arce@gmail.com>
2023-05-11 19:35:59 -0600

    Scaled vs Ceiling analyses

--- 02_Cumulatives/bin/Cumulatives.R
+++ 02_Cumulatives/bin/Cumulatives.R
@@ -8,60 +8,66 @@ library(dplyr)
 
 # Figure options ####
 # Creating color ramp palletes
-Palette2019 <- colorRampPalette(c("skyblue1","navyblue"))
+PaletteSympatric <- colorRampPalette(c("skyblue1","navyblue"))
 PaletteAllopatric <- colorRampPalette(c("lightpink1","red3"))
 
 # Text size in points
 s <- 12
 
-# Reading data ####
-cumulative <- read.csv("../data/Cumulatives.csv", header = T, encoding = "UTF-8")
+# Do you want to use "Scaled" or "Ceiling" data?
+math <- "Ceiling"
+
+# Tidying data ####
+cumulative <- read.csv(paste0("../data/Cumulatives_",math,".csv"), header = T, encoding = "UTF-8")
 colnames(cumulative)[1] <- "Ecology"
-colnames(cumulative)[9] <- "Mechanical-Tactile"
+colnames(cumulative)[7] <- "Mechanical-Tactile"
 
-# Removing one row
-cumulative <- cumulative[cumulative$Population!="CachadasXMaraixDorx",]
-
-# Tidying
-cumulative %>% 
-  gather("Barrier", "Isolation", 8:12) %>%
-  mutate(Barrier = factor(Barrier, levels = unique(Barrier))) -> cumulative
-
-# Selecting prezygotics
+# Removing rows without mechanical measurements and tidying
 cumulative %>%
-  filter(Cross=="G♂E♀" | Cross=="E♂G♀") %>%
-  mutate(Cross = factor(Cross, levels = c("G♂E♀","E♂G♀"))) -> Prezygotics
+  filter(N>0) %>%
+  select(!X & !X.1) %>%
+  gather("Barrier", "Isolation", 6:10) %>%
+  mutate(Barrier = factor(Barrier, levels = unique(Barrier))) %>%
+  mutate(Ecology = factor(Ecology, levels = c("Allopatry", "Sympatry"))) %>%
+  mutate(Population = paste0(Population," - ",N)) %>%
+  select(!N) -> cumulative
+
+# Conspecifics crosses ####
+cumulative %>%
+  filter(Type == "Conspecific crosses") %>%
+  mutate(Cross = factor(Cross, levels = c("E♂E♀","G♂G♀"))) -> Conspecifics
 
 # Creating dataframe of colors
-Prezygotics %>%
-  select(Database, Population) %>%
+Conspecifics %>%
+  select(Ecology, Population) %>%
   unique() %>%
-  arrange(Database, Population) -> Colors
+  arrange(Ecology, Population) -> Colors
 
 # Creating colors lists
-colors2019 <- Palette2019(length(Colors$Database[Colors$Database=="2019"]))
-colorsAllopatric <- PaletteAllopatric(length(Colors$Database[Colors$Database=="Allopatric"]))
+colorsSympatric <- PaletteSympatric(nrow(Colors[Colors$Ecology=="Sympatry",]))
+colorsAllopatric <- PaletteAllopatric(nrow(Colors[Colors$Ecology=="Allopatry",]))
 
 # Merging colors to dataframe
 Colors %>%
-  mutate(colors = c(colors2019, colorsAllopatric)) -> Colors
-rm(colors2019, colorsAllopatric)
+  mutate(colors = c(colorsSympatric, colorsAllopatric)) -> Colors
+rm(colorsSympatric, colorsAllopatric)
 colors <- setNames(Colors$colors, Colors$Population)
 rm(Colors)
 
-# Filtering
-Prezygotics %>%
+# Ploting
+Conspecifics %>%
   ggplot() +
-  facet_wrap(Database ~ Cross, ncol = 2, scales = "free") +
+  facet_wrap(Cross ~ Ecology, ncol = 2, scales = "free") +
   geom_line(aes(x=Barrier, y=Isolation, color=Population, group=Population)) +
-  geom_point(aes(x=Barrier, y=Isolation, color=Population, shape=Database)) +
-  scale_color_manual(values = colors) +
+  geom_point(aes(x=Barrier, y=Isolation, color=Population)) +
+  #scale_color_manual(values = colors) +
   scale_y_continuous(n.breaks = 5, limits = c(0,1)) +
   theme_classic() +
   labs(y="Cumulative Reproductive Isolation") +
   theme(axis.title.x = element_blank(),
         strip.background = element_blank(),
         strip.text = element_text(hjust = 0),
+        panel.grid.major.y = element_line(colour = "grey"),
         text = element_text(family = "serif", size = s),
         axis.text.x = element_text(hjust = 1, angle = 15),
         legend.text = element_text(size = s/2),
@@ -72,54 +78,98 @@ Prezygotics %>%
 p_dml <- rvg::dml(ggobj = p)
 
 # Exporting
-officer::read_pptx("../data/Base_PPTX/PNAS_Tall_Image.pptx") %>%
+officer::read_pptx("../../00_BasePPTX/PNAS_Tall_Image.pptx") %>%
   # specify object and location of object (full size)
   officer::ph_with(p_dml, ph_location_fullsize()) %>%
   # export slide
-  print(target = "../results/01_Prezygotics.pptx")
+  print(target = paste0("../figures/01_Conspecifics_",math,".pptx"))
 
-# Filtering postzygotics
+# Heterospecific crosses ####
+rm(Conspecifics)
 cumulative %>%
-  filter(Generation!="F0") %>%
-  mutate(Cross = factor(Cross, levels=c("E♂H♀","G♂H♀","H♂H♀","H♂E♀","H♂G♀"))) -> postzygotics
+  filter(Type == "Heterospecific crosses") %>%
+  mutate(Cross = factor(Cross, levels = c("E♂G♀","G♂E♀"))) -> Heterospecifics
 
 # Creating dataframe of colors
-postzygotics %>%
-  select(Database, Population) %>%
+Heterospecifics %>%
+  select(Ecology, Population) %>%
   unique() %>%
-  arrange(Database, Population) -> Colors
+  arrange(Ecology, Population) -> Colors
 
 # Creating colors lists
-colors2019 <- Palette2019(length(Colors$Database[Colors$Database=="2019"]))
-colorsAllopatric <- PaletteAllopatric(length(Colors$Database[Colors$Database=="Allopatric"]))
+colorsSympatric <- PaletteSympatric(nrow(Colors[Colors$Ecology=="Sympatry",]))
+colorsAllopatric <- PaletteAllopatric(nrow(Colors[Colors$Ecology=="Allopatry",]))
 
 # Merging colors to dataframe
 Colors %>%
-  mutate(colors = c(colors2019, colorsAllopatric)) -> Colors
-rm(colors2019, colorsAllopatric)
+  mutate(colors = c(colorsSympatric, colorsAllopatric)) -> Colors
+rm(colorsSympatric, colorsAllopatric)
 colors <- setNames(Colors$colors, Colors$Population)
 rm(Colors)
 
-postzygotics %>%
+# Ploting
+Heterospecifics %>%
   ggplot() +
-  facet_grid(rows=vars(Generation), cols = vars(Cross)) +
-  geom_line(aes(x=Barrier, y=Isolation, color=Database, group=Population)) +
-  geom_point(aes(x=Barrier, y=Isolation, color=Database)) +
-  scale_color_manual(values = c('navyblue','red3')) + #'#66c2a5'
+  facet_wrap(Cross ~ Ecology, ncol = 2, scales = "free") +
+  geom_line(aes(x=Barrier, y=Isolation, color=Population, group=Population)) +
+  geom_point(aes(x=Barrier, y=Isolation, color=Population)) +
   #scale_color_manual(values = colors) +
+  scale_y_continuous(n.breaks = 5, limits = c(0,1)) +
   theme_classic() +
   labs(y="Cumulative Reproductive Isolation") +
   theme(axis.title.x = element_blank(),
+        strip.background = element_blank(),
+        strip.text = element_text(hjust = 0),
+        panel.grid.major.y = element_line(colour = "grey"),
         text = element_text(family = "serif", size = s),
         axis.text.x = element_text(hjust = 1, angle = 15),
-        legend.position="bottom") -> p
+        legend.text = element_text(size = s/2),
+        legend.title = element_text(size = s/1.5),
+        legend.position = "bottom") -> p
 
 # Converting to dml
 p_dml <- rvg::dml(ggobj = p)
 
 # Exporting
-officer::read_pptx("../data/Base_PPTX/PNAS_Large_Image.pptx") %>%
+officer::read_pptx("../../00_BasePPTX/PNAS_Tall_Image.pptx") %>%
   # specify object and location of object (full size)
   officer::ph_with(p_dml, ph_location_fullsize()) %>%
   # export slide
-  print(target = "../results/02_Postzygotics.pptx")
+  print(target = paste0("../figures/02_Heterospecifics_",math,".pptx"))
+
+# Postzygotics barriers ####
+rm(Heterospecifics)
+cumulative %>%
+  filter(Type == "Postzygotics") %>%
+  mutate(Cross = factor(Cross, levels = c("E♂H♀","G♂H♀","H♂H♀","H♂E♀","H♂G♀"))) -> Postzygotics
+
+# Ploting
+s <- 10
+Postzygotics %>%
+  ggplot() +
+  facet_grid(rows = vars(Ecology), cols = vars(Cross)) +
+  geom_line(aes(x=Barrier, y=Isolation, color=Population, group=Population)) +
+  geom_point(aes(x=Barrier, y=Isolation, color=Population)) +
+  scale_y_continuous(n.breaks = 5, limits = c(0,1)) +
+  theme_classic() +
+  labs(y="Cumulative Reproductive Isolation") +
+  theme(axis.title.x = element_blank(),
+        strip.background = element_blank(),
+        strip.text = element_text(hjust = 0),
+        panel.grid.major.y = element_line(colour = "grey"), 
+        text = element_text(family = "serif", size = s),
+        axis.text.x = element_text(hjust = 1, angle = 15, size = 6),
+        legend.text = element_text(size = s/3),
+        legend.title = element_text(size = s/2),
+        legend.key.size = unit(1,"line"),
+        legend.position = "bottom") -> p
+
+# Converting to dml
+p_dml <- rvg::dml(ggobj = p)
+
+# Exporting
+officer::read_pptx("../../00_BasePPTX/PNAS_Big_Image.pptx") %>%
+  # specify object and location of object (full size)
+  officer::ph_with(p_dml, ph_location_fullsize()) %>%
+  # export slide
+  print(target = paste0("../figures/03_Postzygotics_",math,".pptx"))
